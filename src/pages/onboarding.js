78b95e994eac149832dc290f94a4f99057135044
ahmadrosid <alahmadrosid@gmail.com>
2021-10-22 15:38:46 +0700

    feat: tidy up onboarding

--- src/pages/onboarding.js
+++ src/pages/onboarding.js
@@ -7,12 +7,12 @@ export default function Onboarding() {
         <div className="grid place-items-center h-screen bg-[#f7f6f3]">
             <div className="space-y-8">
                 <div className="space-y-2 text-center">
-                    <h1 className="font-light">Nocion</h1>
-                    <h2>How are you planning to use Notion?</h2>
-                    <p className="text-[#413e38]">We’ll streamline your setup experience accordingly.</p>
+                    <h1 className="pb-12">🚀</h1>
+                    <h2>How are you planning to use Nocion?</h2>
+                    <p className="text-[#413e38]">Choose one that make you feel rich rather than ones that make you feel poor 😎.</p>
                 </div>
                 <div className="flex justify-between max-w-xl gap-4 cursor-pointer">
-                    <div className={`w-full bg-white p-8 rounded-lg shadow-md border ${teamSelected ? 'border-[#308bbf]' : 'border-white'} text-center relative`} onClick={() => setSelectedTeam(true)}>
+                    <div className={`w-full bg-white p-8 rounded-lg shadow-md border-2 ${teamSelected ? 'border-[#308bbf]' : 'border-white'} text-center relative`} onClick={() => setSelectedTeam(true)}>
                         {teamSelected && (
                             <div className="flex justify-center py-6 absolute top-0 left-0 w-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-[#1D89C7]" viewBox="0 0 20 20" fill="currentColor">
@@ -24,7 +24,7 @@ export default function Onboarding() {
                         <p className="text-base py-2 text-[#808080]">Colaborate on you docs, projects, and wikis.</p>
                         <p className="text-[#777777] py-2 text-sm font-light">Try for free</p>
                     </div>
-                    <div className={`w-full bg-white p-8 rounded-lg shadow-md border ${!teamSelected ? 'border-[#308bbf]' : 'border-white'} text-center relative`} onClick={() => setSelectedTeam(false)}>
+                    <div className={`w-full bg-white p-8 rounded-lg shadow-md border-2 ${!teamSelected ? 'border-[#308bbf]' : 'border-white'} text-center relative`} onClick={() => setSelectedTeam(false)}>
                         {!teamSelected && (
                             <div className="flex justify-center py-6 absolute top-0 left-0 w-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-[#1D89C7]" viewBox="0 0 20 20" fill="currentColor">
