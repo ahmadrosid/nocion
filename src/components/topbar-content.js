35ca0dea3dbc2537b74e1c5bb94ebc79d28ccd56
ahmadrosid <alahmadrosid@gmail.com>
2023-01-21 04:20:17 +0700

    fix: update tailwindcss to version 3.2.4 add sidebar transition on hide

--- src/components/topbar-content.js
+++ src/components/topbar-content.js
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 import useOnClickOutside from "../lib/hooks/useOnClickOutside";
 
-export default function TopbarContent({ title, onUpdateTitle }) {
+export default function TopbarContent({ title, onUpdateTitle, isShowToggle, toggleSidebar }) {
     const refTitleContainer = useRef()
     const refTitle = useRef()
     const [contentTitle, setContentTitle] = useState(title)
@@ -25,9 +25,16 @@ export default function TopbarContent({ title, onUpdateTitle }) {
     return (
         <div className="p-2 flex justify-between">
             <div className="relative" ref={refTitleContainer} >
-                <div onClick={handleUpdateTitle} ref={refTitle} className="hover:bg-hover px-2 py-1 rounded-md cursor-pointer">{contentTitle}</div>
+                <div className="flex items-center">
+                    <p className={`hover:bg-hover px-2 py-1 rounded-md cursor-pointer block ${isShowToggle && 'hidden'}`} onClick={toggleSidebar}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" transform="scale(-1,1)">
+                            <path fillRule="evenodd" d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
+                        </svg>
+                    </p>
+                    <div onClick={handleUpdateTitle} ref={refTitle} className="hover:bg-hover px-2 py-1 rounded-md cursor-pointer">{contentTitle}</div>
+                </div>
                 {showEditTitle && (
-                    <div className="bg-white rounded shadow-lg py-1 px-2 border border-[#dadada] flex items-center gap-2 absolute -left-full w-[350px]">
+                    <div className="bg-white rounded shadow-lg py-1 px-2 border border-[#dadada] flex items-center gap-2 absolute left-0 w-[350px]">
                         <div className="border border-[#dadada] px-1.5 py-1 rounded">
                             <img src="/user.jpg" className="w-6 rounded" />
                         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
+// @ts-check
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+};
